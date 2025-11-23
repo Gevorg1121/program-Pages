53b961e40c5be555f86424c5a9175e514bb2f8ea
Gévorg BABAYAN <gbabayan718@gmail.com>
2025-11-23 11:41:01 +0100

    Program is complete

--- scripte.js
+++ scripte.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+let textForExecuting = "";
+
+function pageCloning(){
+  const PAGE_NUMBER = document.getElementById("page").value;
+  const PAGE_QUANTITY = +document.getElementById("quantity").value;
+
+  for(let i = 0; i  < PAGE_QUANTITY; i++){
+    if(textForExecuting != ""){
+      textForExecuting +="," + PAGE_NUMBER;
+    } else{
+      textForExecuting = PAGE_NUMBER;
+    }
+
+    
+  }
+
+  document.getElementById("result").value = textForExecuting;
+}
